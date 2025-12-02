f2011a6c1a3636002d9e4d8d46802c38e02e32b9
kumi22 <mk@ichdarfdas.ch>
2025-12-02 21:48:48 +0100

    Add word count

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def main():
+    count_words = 0
+    book_as_string = get_book_text("books/frankenstein.txt")
+    num_words = word_counter(book_as_string)
+    # count_words += len(split_words)
+    # print(count_words)
+    print(f"Found {num_words} total words")
+
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+       return f.read()
+  
+
+def word_counter(book_text):
+    word_split = book_text.split()
+    return len(word_split)
+
+main()
