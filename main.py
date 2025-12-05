d969b24e95fc18f3a6263c6a990ec6f14abd2c86
kumi22 <mk@ichdarfdas.ch>
2025-12-05 19:28:53 +0100

    Add buggy code

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from stats import word_counter
+from stats import character_counter
+from stats import sort_chars
+
 
 def main():
-    count_words = 0
     book_as_string = get_book_text("books/frankenstein.txt")
     num_words = word_counter(book_as_string)
+    num_characters = character_counter(book_as_string)
+    sorted_chars = sort_chars(book_as_string)
     # count_words += len(split_words)
     # print(count_words)
     print(f"Found {num_words} total words")
+    print(num_characters)
 
 
 def get_book_text(file_path):
-    with open(file_path) as f:
-       return f.read()
+    with open(file_path) as f:       
+       return f.read().lstrip("\ufeff")
   
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,24 @@
 def word_counter(book_text):
     word_split = book_text.split()
     return len(word_split)
+
+def character_counter(book_text):
+    character_dict = {}
+    for char in book_text:
+        lower_char = char.lower()
+        if lower_char in character_dict:
+            character_dict[lower_char] = character_dict[lower_char] + 1
+        else:
+           character_dict[lower_char] = 1
+    
+    return character_dict
+        
+def sort_chars(character_dict):
+    sorted_list = []
+    for ch, count in character_dict.items():
+        sorted_list.append(characters)
+        if ch.isalpha():
+            sorted_list.append({"char": ch, "num": count})
+    print(sorted_list)
+    return sorted_list
+    
